67df6625030122c222003199d5f463c61f358a7d
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-20 16:02:45 +0300

    Updated Js, css and html

--- main.js
+++ main.js
@@ -49,101 +49,76 @@ function detectSpeed() {
 
 // Net Salary Calculator
 
-window.addEventListener('load', ()=>{
-    // Global variables 
-    const calculateBtn = document.getElementById('calculate')
-    const netSalaryInput = document.getElementById('netInput')
-    const monthlyPeriod = document.getElementById('monthly')
-    const annuallyPeriod = document.getElementById('annually')
-    
-    let grossSalaryDisplay = document.getElementById('grossResult')
-    const kraDisplay = document.getElementById('kra')
-    const nssfDisplay = document.getElementById('nssf')
+function calculateNetSalary() {
+    // get user input for basic salary and benefits
+    let basicSalary = parseFloat(document.getElementById("basicSalary").value);
+    let benefits = parseFloat(document.getElementById("benefits").value);
 
-    const income = []
-    const personalRelief = 28800
-    
-    // Saving the amount into the income array 
-    netSalaryInput.addEventListener('input', ()=>{
-        const incomeSalary = netSalaryInput.value
-        income.push(incomeSalary)
-        return income
-    })
+    // calculate gross salary
+    let grossSalary = basicSalary + benefits;
 
-    //Things to be done when the button is clicked
-    calculateBtn.addEventListener('click', ()=>{
-        // cheking payment period
-        if(annuallyPeriod.checked == true && monthlyPeriod.checked == true){
-            alert("Both monthly and annually can't be checked")
-            return
-        }
+    // calculate payee (KRA)
+    let KRA = 0;
+    if (grossSalary <= 24000) {
+    KRA = 0;
+    } else if (grossSalary <= 40000) {
+    KRA = (grossSalary - 24000) * 0.1;
+    } else if (grossSalary <= 80000) {
+    KRA = 1600 + (grossSalary - 40000) * 0.2;
+    } else if (grossSalary <= 120000) {
+    KRA = 9600 + (grossSalary - 80000) * 0.25;
+    } else if (grossSalary <= 160000) {
+    KRA = 21600 + (grossSalary - 120000) * 0.3;
+    } else {
+    KRA = 38400 + (grossSalary - 160000) * 0.35;
+    }
 
-        // When monthly is checked, it multiplies the amount entered by 12
-        if (monthlyPeriod.checked == true){
-            //makes the annually section to be readonly
-            document.getElementById('annually').disabled = true;
-            let monthlyIncome = (income.slice(-1))
-            let monthlySalary = (monthlyIncome.map(Number) * 12)
+    // calculate NHIF deductions
+    let nhifDeductions = 0;
+    if (grossSalary <= 5999) {
+    nhifDeductions = 150;
+    } else if (grossSalary <= 7999) {
+    nhifDeductions = 300;
+    } else if (grossSalary <= 11999) {
+    nhifDeductions = 400;
+    } else if (grossSalary <= 14999) {
+    nhifDeductions = 500;
+    } else if (grossSalary <= 19999) {
+    nhifDeductions = 600;
+    } else if (grossSalary <= 24999) {
+    nhifDeductions = 750;
+    } else if (grossSalary <= 29999) {
+    nhifDeductions = 850;
+    } else if (grossSalary <= 34999) {
+    nhifDeductions = 900;
+    } else if (grossSalary <= 39999) {
+    nhifDeductions = 950;
+    } else if (grossSalary <= 49999) {
+    nhifDeductions = 1000;
+    } else if (grossSalary <= 59999) {
+    nhifDeductions = 1100;
+    } else if (grossSalary <= 69999) {
+    nhifDeductions = 1200;
+    } else if (grossSalary <= 79999) {
+    nhifDeductions = 1300;
+    } else if (grossSalary <= 89999) {
+    nhifDeductions = 1400;
+    } else if (grossSalary <= 99999) {
+    nhifDeductions = 1500;
+    } else {
+    nhifDeductions = 1700;
+    }
 
-            /* Returns an alert when the user has not entered an amout
-            or when the user enters a value less than 288000*/
-            if(monthlySalary < 288000){
-                alert(`${monthlySalary} is not applicable to taxation`)
-            }
-            /* Calculates both gross salry, nssf, nhif when the amount entered
-            is between 288000 and 388000 */
-            if(monthlySalary < 388000){
-                const kraRate = (0.10 * monthlySalary)
-                const nssfRate = (0.06 * monthlySalary)
-                const grossIncome = (monthlySalary + personalRelief - nssfRate - kraRate)
-                grossSalaryDisplay.textContent = grossIncome
-                kraDisplay.textContent = kraRate
-                nssfDisplay.innerText = nssfRate
-            }
-            /* Calculates both gross salry, nssf, nhif when the amount entered
-            is over 388000 */
-            if(monthlySalary > 388000){
-                const kraRate = (0.30 * monthlySalary)
-                const nssfRate = (0.06 * monthlySalary)
-                const grossIncome = (monthlySalary + personalRelief - nssfRate - kraRate)
-                grossSalaryDisplay.textContent = grossIncome
-                kraDisplay.textContent = kraRate
-                nssfDisplay.innerText = nssfRate
-            }
-        }
-        // Performs several functions when annually is checked
-        if(annuallyPeriod.checked == true){
-            document.getElementById('monthly').disabled = true;
-            let anuallyIncome = income.slice(-1)
-            let annualySalary = (anuallyIncome.map(Number) * 1)
+    // calculate NSSF deductions
+    let nssfDeductions = Math.min(grossSalary * 0.06, 2160);
 
-            /* Returns an alert when the user has not entered an amout
-            or when the user enters a value less than 288000*/
-            if(annualySalary < 288000){
-                alert(`${annualySalary} is not applicable to taxation`)
-            }
+    // calculate net salary
+    let netSalary = grossSalary - KRA - nhifDeductions - nssfDeductions;
 
-            /* Calculates both gross salary, nssf, nhif when the amount entered
-            is between 288000 and 388000 */
-            if(annualySalary > 288000 && annualySalary < 388000){
-                const kraRate = (0.10 * annualySalary)
-                const nssfRate = (0.06 * annualySalary)
-                const grossIncome = (annualySalary + personalRelief - nssfRate - kraRate)
-                grossSalaryDisplay.textContent = grossIncome
-                kraDisplay.textContent = kraRate
-                nssfDisplay.innerText = nssfRate
-            }
-            
-            /* Calculates both gross salary, nssf, nhif when the amount entered
-            is above 388000 */
-            if(annualySalary > 388000){
-                const kraRate = (0.30 * annualySalary)
-                const nssfRate = (0.06 * annualySalary)
-                const grossIncome = (annualySalary + personalRelief - nssfRate - kraRate)
-                grossSalaryDisplay.textContent = grossIncome
-                kraDisplay.textContent = kraRate
-                nssfDisplay.innerText = nssfRate
-            }
-        }
-    })
-})
+    // display results on page
+    document.getElementById("grossSalary").innerHTML = `Gross Salary: KES ${grossSalary.toFixed(2)}`;
+    document.getElementById("KRA").innerHTML = `Payee (KRA): KES ${KRA.toFixed(2)}`;
+    document.getElementById("nhifDeductions").innerHTML = `NHIF Deductions: KES ${nhifDeductions.toFixed(2)}`;
+    document.getElementById("nssfDeductions").innerHTML = `NSSF Deductions: KES ${nssfDeductions.toFixed(2)}`;
+    document.getElementById("netSalary").innerHTML = `Net Salary: KES ${netSalary.toFixed(2)}`;
+}
